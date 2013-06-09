079bfa3c513e981004d0d385746f944f9c9e3123
rajithd <rajithgama@gmail.com>
2013-06-09 09:10:41 +0530

    change sql

--- modules/report-scheduler/src/main/java/com/smartsms/reporsitory/impl/AlertAppRepositoryImpl.java
+++ modules/report-scheduler/src/main/java/com/smartsms/reporsitory/impl/AlertAppRepositoryImpl.java
@@ -3,26 +3,32 @@ package com.smartsms.reporsitory.impl;
 
 import com.smartsms.beans.AlertApplication;
 import com.smartsms.reporsitory.AlertAppRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.support.JdbcDaoSupport;
 
 public class AlertAppRepositoryImpl extends JdbcDaoSupport implements AlertAppRepository {
 
+    private final static Logger logger = LoggerFactory.getLogger(AlertAppRepositoryImpl.class);
+
     @Override
     public void saveAlert(AlertApplication alertApplication) {
+        logger.info("Going to save alert app id [{}]",alertApplication.getAppId());
         final String sql = "INSERT INTO `ALERT_APP`(`ID`,`NAME`,`DESCRIPTION`,`START_DATE`,`END_DATE`) " +
-                "VALUES(:id,:name,:description,;startDate,:endDate)";
+                "VALUES(:id,:name,:description,:startDate,:endDate)";
         NamedParameterJdbcTemplate namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(
                 getJdbcTemplate().getDataSource());
 
         MapSqlParameterSource parameters = new MapSqlParameterSource();
-        parameters.addValue("id",alertApplication.getAppId());
+        parameters.addValue("id",alertApplication.getAppId().toString());
         parameters.addValue("name",alertApplication.getAppName());
         parameters.addValue("description",alertApplication.getAppDescription());
         parameters.addValue("startDate",alertApplication.getStartDate());
         parameters.addValue("endDate",alertApplication.getEndDate());
 
         namedParameterJdbcTemplate.update(sql,parameters);
+        logger.info("Successfully saved alert app id [{}]",alertApplication.getAppId());
     }
 }
