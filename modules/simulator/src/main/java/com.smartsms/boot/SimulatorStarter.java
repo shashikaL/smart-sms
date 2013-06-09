aacf457f1d6fd08f6019cc6bf65e4794e84fb56b
rajithd <rajithgama@gmail.com>
2013-06-09 12:29:12 +0530

    create simulator project, change logs

---
+++ modules/simulator/src/main/java/com.smartsms/boot/SimulatorStarter.java
@@ -0,0 +1,15 @@
+package com.smartsms.boot;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SimulatorStarter {
+
+    private final static Logger logger = LoggerFactory.getLogger(SimulatorStarter.class);
+
+    public static void main(String[] args) {
+        logger.info("================== Starting Simulator ==================");
+        logger.info("================== Simulator Stared ===================");
+    }
+}
