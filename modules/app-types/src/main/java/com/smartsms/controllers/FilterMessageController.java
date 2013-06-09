9db1e922b097b6be99e5a9d4f3bad2c70b6ec486
Niroshan Lakmal Rajapakse <nirooorajapakse@hotmail.com>
2013-06-09 19:06:48 +0530

    added filterContoller

---
+++ modules/app-types/src/main/java/com/smartsms/controllers/FilterMessageController.java
@@ -0,0 +1,11 @@
+package com.smartsms.controllers;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Niro
+ * Date: 6/9/13
+ * Time: 4:01 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class FilterMessageController {
+}
---
+++ modules/core-logic/src/main/java/com/smartsms/beans/FilterMessage.java
@@ -0,0 +1,11 @@
+package com.smartsms.beans;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Niro
+ * Date: 6/9/13
+ * Time: 4:25 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class FilterMessage {
+}
