ef83a10c5f185ac4f9db5e7889904175718374ad
rajithd <rajithgama@gmail.com>
2013-06-24 00:02:22 +0530

    implement twitter integration

--- modules/app-types/src/main/java/com/smartsms/controllers/LoginController.java
+++ modules/app-types/src/main/java/com/smartsms/controllers/LoginController.java
@@ -1,15 +1,72 @@
 package com.smartsms.controllers;
 
+import com.smartsms.openId.config.SpringSocialConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.social.connect.ConnectionFactoryLocator;
+import org.springframework.social.oauth1.AuthorizedRequestToken;
+import org.springframework.social.oauth1.OAuth1Operations;
+import org.springframework.social.oauth1.OAuth1Parameters;
+import org.springframework.social.oauth1.OAuthToken;
+import org.springframework.social.twitter.api.Twitter;
+import org.springframework.social.twitter.api.TwitterProfile;
+import org.springframework.social.twitter.api.impl.TwitterTemplate;
+import org.springframework.social.twitter.connect.TwitterConnectionFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
+    @Autowired
+    private SpringSocialConfig springSocialConfig;
+
+    @Autowired
+    private ConnectionFactoryLocator connectionFactoryLocator;
+
+    @Autowired
+    private OAuth1Parameters twitterOAuth1Parameters;
+
+    private OAuthToken requestToken;
+
     @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public String redirectLogin(){
+    public String redirectLogin(Model model){
+        String twitterAuthUrl = findTwitterAuthUrl();
+        System.out.println("Twitter login =============" + twitterAuthUrl);
         return "login";
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/social-connections/twitter")
+    public String authenticateWithTwitter(@RequestParam("oauth_token") String authToken, @RequestParam("oauth_verifier") String authVerifier){
+        TwitterConnectionFactory twitterConnectionFactory = (TwitterConnectionFactory) connectionFactoryLocator
+                .getConnectionFactory(Twitter.class);
+
+        OAuth1Operations oauthOperations = twitterConnectionFactory
+                .getOAuthOperations();
+
+        OAuthToken accessToken = oauthOperations.exchangeForAccessToken(new AuthorizedRequestToken(requestToken, authVerifier), null);
+
+        Twitter twitter = new TwitterTemplate(springSocialConfig.getTwitterAppKey(),springSocialConfig.getTwitterAppSecret(),accessToken.getValue(),accessToken.getSecret());
+        TwitterProfile userProfile = twitter.userOperations().getUserProfile();
+        System.out.println(userProfile.getFriendsCount());
+        return "redirect:/Home";
+    }
+
+    private String findTwitterAuthUrl(){
+        TwitterConnectionFactory twitterConnectionFactory = (TwitterConnectionFactory) connectionFactoryLocator
+                .getConnectionFactory(Twitter.class);
+
+        OAuth1Operations oauthOperations = twitterConnectionFactory
+                .getOAuthOperations();
+
+        requestToken = oauthOperations.fetchRequestToken(
+                twitterOAuth1Parameters.getCallbackUrl(), null);
+        System.out.println(requestToken.getValue());
+        System.out.println(requestToken.getSecret());
+        return oauthOperations.buildAuthorizeUrl(
+                requestToken.getValue(), null);
 
     }
 }
--- modules/app-types/src/main/java/com/smartsms/openId/config/SpringSocialConfig.java
+++ modules/app-types/src/main/java/com/smartsms/openId/config/SpringSocialConfig.java
@@ -1,12 +1,18 @@
 package com.smartsms.openId.config;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.social.connect.ConnectionFactoryLocator;
+import org.springframework.social.connect.support.ConnectionFactoryRegistry;
+import org.springframework.social.oauth1.OAuth1Parameters;
+import org.springframework.social.twitter.connect.TwitterConnectionFactory;
 
 /**
  * @author shashi
  */
-@Component
+@Configuration
 public class SpringSocialConfig {
 
     @Value("${facebook.oauth.url}")
@@ -18,27 +24,57 @@ public class SpringSocialConfig {
     @Value("${facebook.app.secret}")
     private String facebookAppSecret;
 
-    public String getFacebookLoginUrl() {
-        return facebookLoginUrl;
+    @Value("${twitter.app.secret}")
+    private String twitterAppSecret;
+
+    @Value("${twitter.app.key}")
+    private String twitterAppKey;
+
+    @Value("${twitter.redirect.url}")
+    private String twitterRedirectURL;
+
+
+    @Bean
+    @Scope(value = "singleton")
+    public ConnectionFactoryLocator connectionFactoryLocator() {
+
+        ConnectionFactoryRegistry registry = new ConnectionFactoryRegistry();
+
+        // Register Facebook Connection Factory
+//        registry.addConnectionFactory(new FacebookConnectionFactory(
+//                facebookAppKey, facebookAppSecret));
+
+        // Register LinkedIn Connection Factory
+//        registry.addConnectionFactory(new LinkedInConnectionFactory(
+//                linkedinAppKey, linkedinAppSecret));
+
+        // Register Twitter Connection Factory
+        registry.addConnectionFactory(new TwitterConnectionFactory(
+                twitterAppKey, twitterAppSecret));
+
+        return registry;
     }
 
-    public void setFacebookLoginUrl(String facebookLoginUrl) {
-        this.facebookLoginUrl = facebookLoginUrl;
+    @Bean
+    public OAuth1Parameters twitterOAuth1Parameters() {
+        OAuth1Parameters params = new OAuth1Parameters();
+        params.setCallbackUrl(twitterRedirectURL);
+        return params;
     }
 
-    public String getFacebookAppId() {
-        return facebookAppId;
+    public String getTwitterAppSecret() {
+        return twitterAppSecret;
     }
 
-    public void setFacebookAppId(String facebookAppId) {
-        this.facebookAppId = facebookAppId;
+    public void setTwitterAppSecret(String twitterAppSecret) {
+        this.twitterAppSecret = twitterAppSecret;
     }
 
-    public String getFacebookAppSecret() {
-        return facebookAppSecret;
+    public String getTwitterAppKey() {
+        return twitterAppKey;
     }
 
-    public void setFacebookAppSecret(String facebookAppSecret) {
-        this.facebookAppSecret = facebookAppSecret;
+    public void setTwitterAppKey(String twitterAppKey) {
+        this.twitterAppKey = twitterAppKey;
     }
 }
