5947d35c462e816e4ec7214879c87e3226910c30
snej <hasini.rajamanthrie@gmail.com>
2013-09-01 16:33:38 +0530

    Single Unsubscribtion

--- modules/core-logic/src/main/java/com/smartsms/repo/SubscriberRepository.java
+++ modules/core-logic/src/main/java/com/smartsms/repo/SubscriberRepository.java
@@ -6,4 +6,6 @@ import com.smartsms.beans.Subscribe;
 public interface SubscriberRepository {
 
     public void saveSubscriber(Subscribe subscribe);
+
+    public Subscribe findSubscriber(String unSubscriberNumber);
 }
